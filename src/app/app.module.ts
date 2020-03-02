761e17a552a5dd64c07ef02e8db65e60ef074389
Simon Kardell <simonkardell@gmail.com>
2020-03-02 21:16:37 +0100

    add scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipes/recipe.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './recipes/search/search.component';
+import {StoreModule} from '@ngrx/store';
+
+import * as fromRecipes from './store/reducers/recipe.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({recipe: fromRecipes}),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipes/search/search.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.sass']
+})
+export class SearchComponent implements OnInit {
+
+  data = '';
+  constructor() { }
+
+  onSubmit() {
+    console.log('MUCH SUBMITTING');
+  }
+  ngOnInit(): void {
+  }
+
+}
